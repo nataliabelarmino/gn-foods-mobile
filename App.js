d4364d4f62d7d94fa3844ed02465e486217b3e69
gustavodcb <gustavodavi.gdcb@gmail.com>
2025-02-01 09:39:26 -0300

    IconsRota

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Entypo from '@expo/vector-icons/Entypo';
 import Feather from '@expo/vector-icons/Feather';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+
 
 import TelaLogin from './src/Tela/Telalogin';
 import TelaCriarConta from './src/Tela/TelaCriarConta';
@@ -17,20 +21,22 @@ const Tab = createBottomTabNavigator();
 
 function BottomTab(){
   return(
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={TelaPrincipal} options={{tabBarIcon:({focused, color, size}) =><Entypo name="home" size={24} color="black" />}} />
+    <Tab.Navigator
+    screenOptions={{
+      tabBarStyle: {backgroundColor: '#FEAD10'},
+      tabBarActiveTintColor: 'white',
+      tabBarInactiveTintColor: 'black',
+
+    }}
+    >
+      <Tab.Screen name='Home' component={TelaPrincipal} options={{tabBarIcon:({focused, color, size}) =><MaterialCommunityIcons name="home-outline" size={28} color="black" />}} />
+      <Tab.Screen name='Busca' component={TelaPrincipal} options={{tabBarIcon:({focused, color, size}) =><Entypo name="magnifying-glass" size={24} color="black" />}}/>
       <Tab.Screen name='Pedidos' component={TelaCriarConta} options={{tabBarIcon:({focused, color, size}) =><Feather name="shopping-bag" size={24} color="black" />}} />
-
-
+      <Tab.Screen name='Perfil' component={TelaCriarConta} options={{tabBarIcon:({focused, color, size}) =><MaterialIcons name="person-outline" size={28} color="black" />}} />
     </Tab.Navigator>
-
-
-
-
   );
-
-
 }
+
 
 
 
